527bc7b87b6702ecadf1da06a19384336268636c
Jay <home@mcgaffigan.com>
2011-01-30 20:42:12 -0500

    Adding config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threads::Application.config.secret_token = '35f2769c60aad3111c922ff6e02e874efc7cc66d91931a78e878f3c173dbaddd39f39a128e716a4746a067bba4a324f8c40d30cc8d8479bdecf4c722cad26560'
