fd2552ebccc03498f390e7db765373a819a819c3
Jay <home@mcgaffigan.com>
2011-01-31 22:20:05 -0500

    cleaning up some unit tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,19 +9,10 @@ describe PostsController do
   def mock_post(stubs ={}) 
     @mock_post ||= mock_model(Post, stubs)
   end
+  def mock_parent_post(stubs ={}) 
+    @mock_post ||= mock_model(Post, stubs)
+  end
 
-  describe "GET 'index'" do 
-    it "should be successful" do
-      Comment.stub(:find) {mock_comment}
-      mock_comment.should_receive(:posts).and_return(mock_post)
-      get 'index', :comment_id => 1
-      response.should be_success
-    end
-  end
-  
-  describe "GET 'edit'" do
-    
-  end
   
   describe "POST 'create'" do
     it "should create a first post" do
@@ -29,18 +20,23 @@ describe PostsController do
       x = mock("builder")
       mock_comment.should_receive(:posts).and_return(x)
       x.should_receive("create!").and_return(mock_post)      
-      
+      mock_comment.should_receive(:updated_at=)
+      mock_comment.should_receive(:save!)
       post 'create', :comment_id=> 1, :post => {:body => "Post body", :name => "Poster"}
-      response.should be_success
+      response.should be_redirect
     end
     it "should create a child post" do
       Comment.stub(:find).with(1) {mock_comment}
       x = mock("builder")
       mock_comment.should_receive(:posts).and_return(x)
+      Post.should_receive("find").and_return(mock_parent_post)
+      mock_parent_post.should_receive("children").and_return(x)
       x.should_receive("create!").and_return(mock_post)      
-      
+      x.should_receive("<<")
+      mock_comment.should_receive(:updated_at=)
+      mock_comment.should_receive(:save!)
       post 'create', :comment_id=> 1, :parent_id => 1, :post => {:body => "Post body", :name => "Poster"}
-      response.should be_success
+      response.should be_redirect
       
     end
   end
